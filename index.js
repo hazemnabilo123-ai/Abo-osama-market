8da84528fc08cb5b9353a72179236bb452e40bc5
hazemnabilo123-ai <hazemnabiloo123@gmail.com>
2025-10-30 22:40:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ searchInput.addEventListener("keyup", function() {
     const name = product.textContent.toLowerCase();
     product.style.display = name.includes(input) ? "block" : "none";
   });
+
 });
 
 
 
+// عند الضغط على كلمة المنتجات
+document.querySelector(".dropbtn").addEventListener("click", function() {
+  document.getElementById("menu").classList.toggle("show");
+});
 
+// إغلاق القائمة لو ضغطت في أي مكان براها
+window.addEventListener("click", function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    const dropdown = document.getElementById("menu");
+    if (dropdown.classList.contains("show")) {
+      dropdown.classList.remove("show");
+    }
+  }
+});
 
 
 
